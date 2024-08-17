85bea843d0281816e324c129c542de280513aef8
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-17 21:36:19 +0900

    [Feat/#1] 회원가입 DTO 추가

---
+++ LetsBasketball/Source/Network/Onboarding/JoinQuery.swift
@@ -0,0 +1,14 @@
+//
+//  JoinQuery.swift
+//  LetsBasketball
+//
+//  Created by 강석호 on 8/17/24.
+//
+
+import Foundation
+
+struct JoinQuery: Encodable {
+    let email: String
+    let password: String
+    let nick: String
+}
