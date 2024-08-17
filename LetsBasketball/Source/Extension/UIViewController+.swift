6d0d655090e3f264ac95535a4aee6d10de2923db
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-17 19:52:20 +0900

    [Feat/#1] Alert 함수 추가

--- LetsBasketball/Source/Extension/UIViewController+.swift
+++ LetsBasketball/Source/Extension/UIViewController+.swift
@@ -10,9 +10,16 @@ import UIKit
 extension UIViewController {
     func setRootViewController(_ viewController: UIViewController) {
         if let scene = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate,
-        let window = scene.window {
+           let window = scene.window {
             window.rootViewController = viewController
             UIView.transition(with: window, duration: 0.2, options: [.transitionCrossDissolve], animations: nil, completion: nil)
         }
+    }
+    
+    func showAlert(title: String){
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alertController.addAction(confirmAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
