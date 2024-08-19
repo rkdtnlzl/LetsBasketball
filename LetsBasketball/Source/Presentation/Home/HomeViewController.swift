1111857993dbb17616ff1515c2a8d7e2a92ffab2
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-19 19:45:10 +0900

    [Feat/#5] diffable DataSource로 컬렉션뷰 추가

--- LetsBasketball/Source/Presentation/Home/HomeViewController.swift
+++ LetsBasketball/Source/Presentation/Home/HomeViewController.swift
@@ -8,5 +8,36 @@
 import UIKit
 
 final class HomeViewController: BaseViewController {
+    deinit {
+        print("deinit \(self)")
+    }
     
+    let homeView = HomeView()
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Int>!
+    
+    override func loadView() {
+        self.view = homeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureDataSource()
+        applySnapshot()
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: homeView.collectionView) { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentCollectionViewCell.id, for: indexPath)
+            cell.backgroundColor = .white
+            return cell
+        }
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Array(0..<4))
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
 }
