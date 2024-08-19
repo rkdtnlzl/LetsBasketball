cbd57195f6e75fb5ba8c56fb4d0bea3713930bed
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-19 20:50:51 +0900

    [Fix/#5] RootView 탭바뷰컨트롤러로 수정

--- LetsBasketball/Source/Presentation/TabBar/TabBarViewController.swift
+++ LetsBasketball/Source/Presentation/TabBar/TabBarViewController.swift
@@ -18,15 +18,15 @@ class TabBarController: UITabBarController {
         
         let home = HomeViewController()
         let nav1 = UINavigationController(rootViewController: home)
-        nav1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        nav1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), tag: 0)
         
         let favorite = FavoriteViewController()
         let nav2 = UINavigationController(rootViewController: favorite)
-        nav2.tabBarItem = UITabBarItem(title: "관심 게시글", image: UIImage(systemName: "heart.fill"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "관심 게시글", image: UIImage(systemName: "heart"), tag: 1)
         
         let profile = ProfileViewController()
         let nav3 = UINavigationController(rootViewController: profile)
-        nav3.tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "person.fill"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "person"), tag: 2)
         
         setViewControllers([nav1,nav2,nav3], animated: true)
     }
