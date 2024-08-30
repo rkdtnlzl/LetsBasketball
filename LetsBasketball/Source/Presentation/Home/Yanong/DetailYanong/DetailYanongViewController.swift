c6c757cc8969234f53cad0b222528f7ebaafaa96
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-30 16:29:52 +0900

    [Feat/#17] 좋아요 Toggle UserDefaults에 저장

--- LetsBasketball/Source/Presentation/Home/Yanong/DetailYanong/DetailYanongViewController.swift
+++ LetsBasketball/Source/Presentation/Home/Yanong/DetailYanong/DetailYanongViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import RxSwift
 
 final class DetailYanongViewController: BaseViewController {
     deinit {
         print("deinit \(self)")
     }
     
+    let viewModel = DetailYanongViewModel()
     let detailYanongView = DetailYanongView()
     
     var latitude: String = ""
@@ -21,8 +23,40 @@ final class DetailYanongViewController: BaseViewController {
         self.view = detailYanongView
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
+    override func configureNavigation() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: nil)
+    }
+    
     override func configureTarget() {
         detailYanongView.mapButton.addTarget(self, action: #selector(mapButtonClicked), for: .touchUpInside)
+    }
+    
+    private func bind() {
+        let input = DetailYanongViewModel.Input(heartButtonClicked: navigationItem.rightBarButtonItem!.rx.tap)
+        
+        let output = viewModel.transform(input: input)
+        
+        output.isLiked
+            .observe(on: MainScheduler.instance)
+            .bind(with: self, onNext: { owner, isLiked in
+                let imageName = isLiked ? "heart.fill" : "heart"
+                owner.navigationItem.rightBarButtonItem?.image = UIImage(systemName: imageName)
+            })
+            .disposed(by: disposeBag)
+        
+        output.error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                if let error = error {
+                    print(error)
+                }
+            })
+            .disposed(by: disposeBag)
     }
     
     @objc func mapButtonClicked() {
